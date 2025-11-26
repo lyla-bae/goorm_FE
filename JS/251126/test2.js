dad5b3938824d182637d54f6c065ddce0384a7c5
lyla <bgy09270@naver.com>
2025-11-26 14:24:09 +0900

    251126 JS 수업실습

--- JS/251126/test2.js
+++ JS/251126/test2.js
@@ -1,19 +1,19 @@
-// 1. 아래 Promise chaining 방식을 async, await로 작성하세요
+// // 1. 아래 Promise chaining 방식을 async, await로 작성하세요
 
-// fetch("https://jsonplaceholder.typicode.com/posts/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
+// // fetch("https://jsonplaceholder.typicode.com/posts/1")
+// //   .then((response) => {
+// //     return response.json();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //   });
 
-async function test1() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-  const data = await response.json();
-  console.log(data);
-}
-test1();
+// async function test1() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//   const data = await response.json();
+//   console.log(data);
+// }
+// test1();
 
 // 2.  아래 Promise chaining 방식을 async, await로 작성하세요
 
@@ -24,3 +24,9 @@ fetch("https://jsonplaceholder.typicode.com/comments")
   .then((data) => {
     console.log(data);
   });
+
+async function test2() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const data = await response.json();
+  console.log(data);
+}
