57a1d30751a76a95252c86ead59c1e67c1bc6872
lyla <bgy09270@naver.com>
2025-11-28 16:56:31 +0900

    251128 NEXT 스터디

--- team-study2/ONEBITE-NEXT/section02/src/pages/_app.tsx
+++ team-study2/ONEBITE-NEXT/section02/src/pages/_app.tsx
@@ -1,11 +1,19 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { NextPage } from "next";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <header>글로벌 헤더</header>
-      <Component {...pageProps} />
-    </>
-  );
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page); // getLayout 뱉을거없으면 그냥 컴포넌트 뱉어라
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
--- team-study2/ONEBITE-NEXT/section02/src/pages/search/index.tsx
+++ team-study2/ONEBITE-NEXT/section02/src/pages/search/index.tsx
@@ -1,7 +1,19 @@
 import { useRouter } from "next/router";
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book.item";
 
 export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
